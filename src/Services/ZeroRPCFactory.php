e0c2e0ea828657ae8ffe4aa40ebf21db1ebca839
Tomas Joelsson <tomas@joelsson.info>
2021-01-26 15:53:49 +0800

    Lazy connect

--- src/Services/ZeroRPCFactory.php
+++ src/Services/ZeroRPCFactory.php
@@ -21,11 +21,13 @@ class ZeroRPCFactory
         }
 
         $context = $context ?: $this->context;
-        $client = new Client($serviceName, $version, $context);
-        if ($timeout == null) {
-            $timeout = env('DEFAULT_RPC_TIMEOUT', 1000);
-        }
-        $client->setTimeout($timeout);
+
+        $client = new _LazyClientWrapper(
+            $serviceName,
+            $version,
+            $context,
+            $timeout
+        );
 
         self::$_clients[$key] = $client;
 
@@ -36,5 +38,46 @@ class ZeroRPCFactory
     {
         $key = $serviceName . $version . $timeout;
         unset(self::$_clients[$key]);
+    }
+}
+
+/*
+ * When a client object is created, the PHP client will immediately connect
+ * to the endpoint. This wrapper avoids connecting until there is an actual
+ * function call.
+ */
+class _LazyClientWrapper
+{
+    private $_client = null;
+    private $_serviceName;
+    private $_version;
+    private $_context;
+    private $_timeout;
+
+    public function __construct($serviceName, $version, $context, $timeout)
+    {
+        $this->_serviceName = $serviceName;
+        $this->_version = $version;
+        $this->_context = $context;
+        $this->_timeout = $timeout;
+    }
+
+    public function __call(string $name, array $arguments)
+    {
+        if (is_null($this->_client)) {
+            $this->_client = new Client(
+                $this->_serviceName,
+                $this->_version,
+                $this->_context
+            );
+
+            if ($this->_timeout == null) {
+                $this->_timeout = env('DEFAULT_RPC_TIMEOUT', 1000);
+            }
+
+            $this->_client->setTimeout($this->_timeout);
+        }
+
+        return call_user_func_array([$this->_client, $name], $arguments);
     }
 }
